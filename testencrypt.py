9a1cce62b5035aa21655777d186956c54ee0cf6a
PLAIS Lionel <lionel.plais@ilp-web.net>
2023-05-24 23:56:03 +0200

    File useless.

--- testencrypt.py
+++
@@ -1,9 +0,0 @@
-import pytest
-import subprocess
-
-subprocess.call([
-	"python3",
-	"encrypt.py",
-	"--video", "test/La vérité sur notre société-Mr Robot [tJaLHsPDuQ4].mp4",
-	"--binary", "test/free-software-song.ogg",
-	"--output", "test/output.mp4", "--verbose"])
